897b80071bf8d0ced690097d31a6e598ad48ff84
Carlos Apolaya <temis_it@hotmail.com>
2023-05-15 12:44:14 -0500

    Sengudo Cambio

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll", function() {
   var navbar = document.querySelector(".navbar");
   navbar.classList.toggle("navbar-scrolled", window.scrollY > 0);
 });
+
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
